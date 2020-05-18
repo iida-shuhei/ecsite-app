723e12d77875d2dbdd7618b80ab5ae9345b0e40b
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-18 19:35:25 +0900

    商品一覧直した

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,40 +1,42 @@
 <template>
   <v-container class="box">
-      <v-row>
-        <v-col v-for="item in this.$store.state.itemList" :key="item.id">
-          <router-link :to="{ name: 'ItemDetail', params : { itemId: item.id }}" class="link">
-            <v-hover v-slot:default="{ hover }">
-              <v-card
-                max-width="344"
-                class="mx-auto"
-                :elevation="hover ? 16 : 2"
-              >
-                <v-img
-                  :src="require('@/assets/images/' + item.imagePath)"
-                ></v-img>
-                
+    <v-row>
+      <v-col cols=4 v-for="item in this.$store.state.itemList" :key="item.id">
+        <router-link :to="{ name: 'ItemDetail', params : { itemId: item.id }}" class="link">
+          <v-hover v-slot:default="{ hover }">
+            <v-card
+              class="mx-auto"
+              max-width="400"
+              :elevation="hover ? 16 : 2"
+            >
+
+              <v-img
+                :src="require('@/assets/images/' + item.imagePath)"
+              ></v-img>
+
+              <v-card-text>
+                {{item.name}}
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+              </v-card-text>
+
+              <v-card-actions>
+
                 <v-card-text>
-                  {{item.name}}
-                  <v-btn icon>
-                    <v-icon>mdi-heart</v-icon>
-                  </v-btn>
+                  {{"[M]" + item.priceM.toLocaleString() + "円(税抜)"}}
+                </v-card-text>
+
+                <v-card-text>
+                  {{"[L]" + item.priceL.toLocaleString() + "円(税抜)"}}
                 </v-card-text>
               
-                <v-card-actions>
-                  <v-card-text>
-                    {{"[M]" + item.priceM.toLocaleString() + "円(税抜)"}}
-                  </v-card-text>
-
-                  <v-card-text>
-                    {{"[L]" + item.priceL.toLocaleString() + "円(税抜)"}}
-                  </v-card-text>
-                
-                </v-card-actions>
-              </v-card>
-            </v-hover>
-          </router-link>
-        </v-col>
-      </v-row>
+              </v-card-actions>
+            </v-card>
+          </v-hover>
+        </router-link>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -49,6 +51,7 @@ export default {
   created() {
     axios.get("http://localhost:8080/item/showItems")
       .then((response) => {
+    console.log(response.data)
         this.setItemList(response.data)
       });
   },
